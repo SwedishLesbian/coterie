db2fbaadeebfbd5b29d7dc9a61e9159cdfcd6e22
mari-bb <128619563+mari-bb@users.noreply.github.com>
2025-04-14 12:45:51 -0400

    Create urls.py
    utilizing django's built in authentication

---
+++ .github/workflows/character_database/urls.py
@@ -0,0 +1,11 @@
+# character_database/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('characters.urls')),  # include app URLs
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+]
