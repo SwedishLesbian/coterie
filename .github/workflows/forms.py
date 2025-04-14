15530f927282b9d76bc49674cd8a9522ace824b8
mari-bb <128619563+mari-bb@users.noreply.github.com>
2025-04-14 10:36:01 -0400

    Create forms.py
    inline comment. update per models.py

---
+++ .github/workflows/forms.py
@@ -0,0 +1,9 @@
+# characters/forms.py
+from django import forms
+from .models import CharacterSheet
+
+class CharacterSheetForm(forms.ModelForm):
+    class Meta:
+        model = CharacterSheet
+      # update fields to match models.py
+        fields = ['name', 'character_class', 'race', 'level', 'attributes']
